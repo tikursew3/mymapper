90260f780aa8b6e0b0fb9e5f9efa216ea1a2dd6e
tikursew3 <97490651+tikursew3@users.noreply.github.com>
2022-06-21 18:52:13 -0500

    Update mapper.sql
    this is my first update on the GitHub repository

--- sql/mapper.sql
+++ sql/mapper.sql
@@ -10,3 +10,26 @@
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
+
+CREATE TABLE IF NOT EXISTS db_name.master_data (Topic varchar(10),
+English varchar(25), Telugu varchar(33),
+Hindi varchar(6)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;;
+
+INSERT INTO `db_name`.`master_data` (`Topic`, `English`, `Telugu`, `Hindi`) 
+VALUES ('vegetables', 'Alternanthera sessilis', 'పొన్నగంటి కూర', 'TBD'), 
+('vegetables', 'Raw tamarind', 'చింతకాయలు', 'TBD'), 
+('vegetables', 'Almond', 'బాదంపప్పు', 'TBD'), 
+('vegetables', 'Amaranthus', 'తోటకూర', 'TBD'), 
+('Fruits', 'Ambarella', 'అడవి మామిడి', 'TBD'), 
+('vegetables', 'Amla', 'ఉసిరి కాయ', 'TBD'), 
+('vegetables', 'Amorphophallus sylvaticus', 'వజ్రకంద', 'TBD'), 
+('Fruits', 'Amra', 'అడవి మామిడి', 'TBD'), 
+('vegetables', 'Angular gourd', 'బీర కాయ', 'TBD'), 
+('Fruits', 'Apple', 'ఆపిల్', 'TBD'), ('Fruits', 'Apple', 'సీమ రేగు  ', 'TBD'),
+('Fruits', 'Apricot', 'సీమ బాదం, జల్లారు పండు', 'TBD'), 
+('vegetables', 'arrow root', 'పాలగరుడ', 'TBD'), 
+('vegetables', 'Artichoke', 'ఆర్టిచూక్', 'TBD'), 
+('vegetables', 'Arum', '\nచేమ దుంప', 'TBD'), 
+('vegetables', 'Ash gourd', 'బూడిద గుమ్మడి కాయ', 'TBD'), 
+('vegetables', 'Asparagus', 'తోట తేగ', 'TBD'), 
+('vegetables', 'Asparagus', 'నేలతాడి గడ్డలు', 'TBD'), ('vege[...]
